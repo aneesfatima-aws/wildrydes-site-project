1115f90294abe581f2fef0782abb7ce060d6b0b4
aneesfatima-aws <aneesfatima0aws@gmail.com>
2025-11-23 01:54:22 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. ap-south-1_ynQP1smMe
-        userPoolClientId: '', // e.g. 13h40rjs8hb45d2i78d5v5d73j
-        region: '' // e.g. ap-south-1
+        userPoolId: 'ap-south-1_ynQP1smMe',
+        userPoolClientId: '13h40rjs8hb45d2i78d5v5d73j',
+        region: 'ap-south-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ' https://dveq8uqp4b.execute-api.ap-south-1.amazonaws.com/dev',
     }
 };
+
 
 
 
